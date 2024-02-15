4eba1967c33976a2b121c10b00657fcd90d87c03
Xander.A <114076297+XanderPxl@users.noreply.github.com>
2024-02-15 21:22:12 +0100

    Main.cpp with comments and perrors

--- main.cpp
+++ main.cpp
@@ -17,12 +17,27 @@
 
 int main(int argc, char *argv[])
 {
+    /*
+     * Create a QT application that will be used for all the gui elements
+     * and create an instance of the TcpClient class
+     */
     QApplication a(argc, argv);
 
     TcpClient client;
 
-    if (argc >= 3)
-        client.firstConnect(argv[1], atoi(argv[2]));
+    /*
+     * If 3 or more arguments (IP addresses, ports, name, etc.) are given
+     * then proceed but print a warning if there are less then 3 arguments given
+     */
 
+    if (argc >= 3)
+    {
+        client.firstConnect(argv[1], atoi(argv[2]));
+    }
+    else
+    {
+        qWarning() << "ERROR: Not enough arguments were given";
+    }
+    // start the QT event loop (user input, network events, other events...)
     return a.exec();
 }
